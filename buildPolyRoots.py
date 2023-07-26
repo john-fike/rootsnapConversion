816ded830e6c2af98a07b6e0d7a1f2e2ce7c82e1
john-fike <johnfike223@gmail.com>
2023-07-26 12:48:02 -0600

    kinda works but not really

--- buildPolyRoots.py
+++ buildPolyRoots.py
@@ -1,56 +1,23 @@
 import extractRSProots
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
 
 
 # folderPath = './samplePoints.xml'
 
 scanDict = extractRSProots.extractRootCoords('./samplePoints.xml') 
-# print(scanDict)
-
-with open('./output.txt','w') as oup:
-    for dict in scanDict:
-        for key, value in dict.items():
-            oup.write(f"{key}: {value}\n")
-
-with open('./output.txt', 'a') as oup:
-    for i in range(len(scanDict[0]["points"])):
-            for j in range(len(scanDict[0]["points"][i])):
-                oup.write(scanDict[0]["points"][i][j])
-
-# with open('./testCVATAnnotations.xml', 'a') as oup:
-image = ET.Element("image")
 
 
-for i in range(len(scanDict[0]["points"])):
-    # for j in range(len(scanDict[0]["points"][i])):
-        # for root in scanDict[0]["points"][i]:
-    pointStr = ''.join(scanDict[0]["points"][i])
-    pointStr = pointStr[:-1]
-    print(pointStr)
-    polyline = ET.Element("polyline", label = "root", source = "manual", occluded = "0", points = pointStr)
-    polyline.text = "\n"
-    image.append(polyline)
+with open('./testCVATAnnotations.xml','r') as f:
+    data = f.read()
+soup = BeautifulSoup(data, 'xml')
 
-
-annotations = ET.ElementTree(image)
-annotations.write('./testCVATAnnotations.xml', encoding='utf-8', xml_declaration=True)
-
-
-
-# for i in range(len(scanDict[0]["rootXVals"])):
-#     for j in range(len(scanDict[0]["rootXVals"][i])):
-#         print(scanDict[0]["rootYVals"][i][j]) 
-#         # print(scanDict[0]["rootXVals"][i][j])
-#         print(",") 
-
-
-
-# for filePath in xmlFiles:
-#     try:
-#         with open('./output.txt','a') as oup:
-#             for dict in scanDict:
-#                 for key, value in dict.items():
-#                     oup.write(f"{key}: {value}\n")           
-#     except Exception as e:
-#         print(f"Error while processing {filePath}: {e}")
+for s in range(len(scanDict)):
+    for i in range(len(scanDict[s]["points"])):
+        image = soup.find("image", attrs = {'id':s})#replace 0 with s variable from dict loop
+        pointStr = ''.join(scanDict[0]["points"][i])
+        pointStr = pointStr[:-1]
+        polyline = soup.new_tag("polyline", "\n", label = "root", source = "manual", occluded = "0", points = pointStr)
+        image.append(polyline)
+with open('./testCVATAnnotations.xml', 'w') as f:
+    f.write(str(soup))
