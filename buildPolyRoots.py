894f3007ade33979675c3be5db87af93145dd518
john-fike <johnfike223@gmail.com>
2023-07-29 11:14:12 -0600

    add gitignore for rsp xml jpg

--- buildPolyRoots.py
+++ buildPolyRoots.py
@@ -4,34 +4,39 @@ from bs4 import BeautifulSoup
 import os
 import glob
 
-folderPath = './'
+##path to file containing rsp. extracted xml will be put here as well
+folderPath = './rsp/'
 
-rspFiles = glob.glob(os.path.join(folderPath, "*.rsp"))
+## grab all rsp files and convert them to xml
+# rspFiles = glob.glob(os.path.join(folderPath, "*.rsp"))
+# for filePath in rspFiles:
+#     extractRSProots.convertRSPtoXML(filePath)
 
-
+#grab all the xml files you just made and create dictionaries with 
+#image name and associated roots
 xmlFiles = glob.glob(os.path.join(folderPath, "*.rsp_clipped.xml"))
-
+missingImageCounter = 0
+missingImageList = []
 for filePath in xmlFiles:
+    print("Inserting polylines...")
     try:
         scanDict = extractRSProots.extractRootCoords(filePath) 
-
-        with open('./testCVATAnnotations.xml','r') as f:
+        with open('annotations.xml','r') as f:
             data = f.read()
         soup = BeautifulSoup(data, 'xml')
 
         for s in range(len(scanDict)):
             for i in range(len(scanDict[s]["points"])):
-                print(scanDict[s]["scanID"])
                 image = soup.find("image", attrs = {'name':scanDict[s]["scanID"]})
-                # print(image)
                 pointStr = ''.join(scanDict[s]["points"][i])
                 pointStr = pointStr[:-1]
-                polyline = soup.new_tag("polyline", "\n", label = "root", source = "manual", occluded = "0", points = pointStr)
+                polyline = soup.new_tag("polylines", "", label = "root", source = "manual", occluded = "0", points = pointStr)
                 image.append(polyline)
-        with open('./testCVATAnnotations.xml', 'w') as f:
+        with open('./annotations.xml', 'w') as f:
             f.write(str(soup))
     except AttributeError as nf:
-        print("Could not find scan:" + scanDict[s]["scanID"])
+        print("WARNING: could not find scan:" + scanDict[s]["scanID"])
+        print("Total missing scans: " + missingImageCounter)
     except Exception as e:
         print("An error occured while writing to CVAT annotation file:", e)
 
