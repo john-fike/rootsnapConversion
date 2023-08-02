01bec582017fb56c7eddc2def1f65e61ac1417bb
john-fike <johnfike223@gmail.com>
2023-08-02 17:38:52 -0600

    add image size graphs, bad image alert

--- rspPngExtractor.py
+++ rspPngExtractor.py
@@ -1,3 +1,7 @@
+import glob
+import os
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 def extract_pngs_from_text_file(file_path):
     with open(file_path, 'rb') as f:
@@ -9,6 +13,9 @@ def extract_pngs_from_text_file(file_path):
     image_count = 0
     start_pos = 0
     saveCount = 0
+
+    imageSizes = []
+    allImageSizes = []
     while True:
         start_pos = data.find(start_marker, start_pos)
         if start_pos == -1:
@@ -20,20 +27,51 @@ def extract_pngs_from_text_file(file_path):
 
         end_pos += 4  # Add 4 bytes for the "IEND" chunk
 
+        image = data[start_pos:end_pos]
+        allImageSizes.append(len(image))
+
         if ((image_count - 1) % 4 == 0):
             saveCount += 1
             image = data[start_pos:end_pos]
+            imageSizes.append(len(image))
             fileName = f.name
-            DIndex = fileName.find("_") + 5
-            imageName = fileName[:DIndex] + "D" + str(saveCount) + "_" + fileName[DIndex:-3] + "PNG"
-            with open(imageName, 'wb') as image_file:
-                image_file.write(image)
+            if(len(image)<10000000):
+                print(fileName + "Is all fucked up!")
+            # DIndex = fileName.find("_") + 5
+            # imageName = "./adjustedImages" + fileName[5:DIndex] + "D" + str(saveCount) + "_" + fileName[DIndex:-3] + "PNG"
+            # with open(imageName, 'wb') as imageFile:
+            #     imageFile.write(image)
 
         image_count += 1
         start_pos = end_pos
+    return [imageSizes, allImageSizes]
+        
 
-# Example usage:
-file_path = "./Barley_101_10Aug2020.rsp"
-extract_pngs_from_text_file(file_path)
+imageSizes = []
+allImageSizes = []
 
+folderPath = './rsp'
+xmlFiles = glob.glob(os.path.join(folderPath, "*.rsp"))
+for filepath in tqdm(xmlFiles):
+    imsz = extract_pngs_from_text_file(filepath)
+    imageSizes.append(imsz[0].copy())
+    allImageSizes.append(imsz[1])
+
+transposed_values = list(zip(*imageSizes))
+flattened_image_sizes = [size for sublist in imageSizes for size in sublist]
+plt.plot(range(len(flattened_image_sizes)), flattened_image_sizes, marker='o', linestyle='-')
+plt.xlabel('Image')
+plt.ylabel('Image Size')
+plt.title('Image Sizes in CVAT')
+plt.grid(True)
+plt.show()
+
+transposed_allValues = list(zip(*allImageSizes))
+flattened_all_image_sizes = [size for sublist in allImageSizes for size in sublist]
+plt.plot(range(len(flattened_all_image_sizes)), flattened_all_image_sizes, marker='o', linestyle='-')
+plt.xlabel('Image')
+plt.ylabel('Image Size')
+plt.title('Image Sizes in RSP Files')
+plt.grid(True)
+plt.show()
 
